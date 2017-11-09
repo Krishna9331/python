65a5ea01ca156b9380cec1ab4e0409863e9af1c5
First Last <kkant551@gmail.com>
2017-11-09 22:01:47 +0530

    A Good first program

---
+++ chapter1/excercise1.py
@@ -0,0 +1,5 @@
+#Excercise from chapter 1
+print('Warning!')
+print()
+print('An "octothorpe" is also called a "pound", "hash", "mesh", or any number of names. Pick the one that makes you chill out.' +
+'Excerpt From: Unknown. "Learn Python 3 the Hard Way: A Very Simple Introduction to the Terrifyingly Beautiful World of Computers and Code." iBooks.')
