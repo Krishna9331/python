ac0fd99691f91f4d7b186683c4710121f7967d81
First Last <kkant551@gmail.com>
2017-11-27 22:51:19 +0530

    Escape sequence in python

---
+++ chapter10/ex10.py
@@ -0,0 +1,19 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'am \\ a \\ cat."
+fat_cat = """
+I'll do a list.
+\t* Cat food
+\t* Fishies.
+\t* Catnip\n\t* Grass
+"""
+silly_cat='''
+I am ' cat.
+I do not need escape sequence.
+'''
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(silly_cat)
